a9207e923cbc9ad770e4c2e049f24a24a7af2ae9
jesse <jsoldat@hotmail.com>
2017-01-31 16:30:16 +0700

    created some routes

---
+++ application/views/templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ciBlog</title>
+	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/flatly/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+				<a href="#" class="navbar-brand">ciBlog</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+ 
