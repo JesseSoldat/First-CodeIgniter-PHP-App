e52aab3806678664a40b64941e92556ce0b0d7f5
jesse <jsoldat@hotmail.com>
2017-01-31 19:08:16 +0700

    created a single page view

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<title>ciBlog</title>
 	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/flatly/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style.css">
 </head>
 <body>
 	<nav class="navbar navbar-inverse">
@@ -14,12 +15,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-				<a href="#" class="navbar-brand">ciBlog</a>
+				<a href="<?php echo base_url(); ?>" class="navbar-brand">ciBlog</a>
 			</div>
 			<div id="navbar">
 				<ul class="nav navbar-nav">
 					<li><a href="<?php echo base_url(); ?>">Home</a></li>
 					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
 					
 				</ul>
 			</div>
